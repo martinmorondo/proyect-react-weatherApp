ab677551bfad39f2b51e36b6a894fc72825ed60f
Martín Morondo <martinmorondo@gmail.com>
2022-11-04 16:04:34 -0300

    changes

--- src/services/creds.js
+++ src/services/creds.js
@@ -1,2 +1,4 @@
-export let apiCity = '95a328d5d0msh7139ed9d43c526bp183634jsnea117057e63b';
-export let apiWeather =  "7ac5bd23405fc2c7039487736e72cc73";
+export const apiCity = '95a328d5d0msh7139ed9d43c526bp183634jsnea117057e63b';
+export const apiWeather =  "7ac5bd23405fc2c7039487736e72cc73";
+
+
