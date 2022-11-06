36aa7cb92b3978b0e3f655cd3e54799234431b43
Martín Morondo <martinmorondo@gmail.com>
2022-11-06 13:23:36 -0300

    add new features

--- src/services/creds.js
+++ src/services/creds.js
@@ -1,4 +1,4 @@
-export const apiCity = '#';
-export const apiWeather =  "#";
+export  const apiCity = '95a328d5d0msh7139ed9d43c526bp183634jsnea117057e63b';
+export  const apiWeather =  "7ac5bd23405fc2c7039487736e72cc73";
 
 
