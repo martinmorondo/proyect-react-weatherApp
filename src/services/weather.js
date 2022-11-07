77c352286ea65054fb6c42415e2ff2f9c3dab3c0
Martín Morondo <martinmorondo@gmail.com>
2022-11-07 14:54:05 -0300

    .

--- src/services/weather.js
+++ src/services/weather.js
@@ -3,10 +3,10 @@ import { ajax } from "../tools/ajax";
 export const getCityWeather = async city => {
     const optionsRequest = {
         method: "GET",
-        url: "https://api.openweathermap.org/data/3.0/onecall",
+        url: `https://api.openweathermap.org/data/2.5/weather`,
         params: {
             q: city,
-            appid:  `${keyWeather}`,
+            appid: `${apiWeather}`,
             units: "metric" // Degrees Celsius.
         }
     };
