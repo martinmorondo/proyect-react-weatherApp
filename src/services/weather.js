ba3a283efd8e1a05be3bad3652452fc0d91cbdce
Martín Morondo <martinmorondo@gmail.com>
2022-11-07 14:56:30 -0300

    change variable name

--- src/services/weather.js
+++ src/services/weather.js
@@ -6,7 +6,7 @@ export const getCityWeather = async city => {
         url: `https://api.openweathermap.org/data/2.5/weather`,
         params: {
             q: city,
-            appid: `${apiWeather}`,
+            appid: `${keyWeather}`,
             units: "metric" // Degrees Celsius.
         }
     };
